23ad0c99dca1d9632412f9d7d2facbc6ef08d998
Jean-Charles Marteau <JMarteau@Bottomline.com>
2020-01-07 14:36:44 +0100

    First set of files toward spring boot

---
+++ src/main/java/com/boot/App.java
@@ -0,0 +1,10 @@
+package com.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
